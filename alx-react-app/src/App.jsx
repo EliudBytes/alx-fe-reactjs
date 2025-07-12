fad834930f67fea8751f5ae53ae61d133f49aae5
Eliud Mathu <mathueliud1@gmail.com>
2025-07-12 07:36:25 -0700

    Task 3: Add UserProfile component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,15 +1,16 @@
 // src/App.jsx
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
-    <>
-      <Header />
-      <MainContent />
-      <Footer />
-    </>
+    <div>
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
+    </div>
   );
 }
 
